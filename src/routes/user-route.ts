1947252a1b87bc54c123c59aada721999faf4cdc
vongsatorn <vongsatorn.jo@gmail.com>
2024-07-20 22:01:18 +0700

    Add script tsss

--- src/routes/user-route.ts
+++ src/routes/user-route.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express, { Router } from "express";
 import { getAll, getUserById } from "../controllers";
 const authenticateMiddleware = require("../middlewares/authticate");
-const router = express.Router();
+const router: Router = express.Router();
 
-router.get("/", getAll);
-router.get("/:id", authenticateMiddleware, getUserById);
+router.use("/", getAll);
+router.use("/:id", authenticateMiddleware, getUserById);
 
 export { router as default };
