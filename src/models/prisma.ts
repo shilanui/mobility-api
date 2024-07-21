f28048aaaa297be219f7864dfd9fb2c5663ea610
vongsatorn <vongsatorn.jo@gmail.com>
2024-07-22 01:27:02 +0700

    Change env db

--- src/models/prisma.ts
+++ src/models/prisma.ts
@@ -1,5 +1,3 @@
-// const { PrismaClient } = require("@prisma/client");
-// const { PrismaClient } = require("../../node_modules/.prisma/client");
 import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
