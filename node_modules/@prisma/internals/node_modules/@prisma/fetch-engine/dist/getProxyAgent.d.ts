c1662c1ec10a3a31aab90d29e0e562c9f5adde81
VongsatornBenz <benz.jongcharoenratanakul@remobie.com>
2026-04-04 23:57:38 +0700

    Fix/git-ignore

--- node_modules/@prisma/internals/node_modules/@prisma/fetch-engine/dist/getProxyAgent.d.ts
+++
@@ -1,3 +0,0 @@
-import { HttpProxyAgentOptions } from 'http-proxy-agent';
-import { HttpsProxyAgentOptions } from 'https-proxy-agent';
-export declare function getProxyAgent(url: string): HttpProxyAgentOptions<string> | HttpsProxyAgentOptions<string> | undefined;
